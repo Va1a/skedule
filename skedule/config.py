425b5a9af0f263e40dbf0727e2aab0e16981c1db
Va1a <n/a>
2024-12-09 19:53:14 -0800

    update env variable names.

--- skedule/config.py
+++ skedule/config.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 
 class Config:
-	SECRET_KEY = os.environ.get('SKEDULE-SECRET-KEY')
+	SECRET_KEY = os.environ.get('SKEDULE_SECRET_KEY')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
-	RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA-PUBLIC-KEY')
-	RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA-SECRET-KEY')
+	RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
+	RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 	RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
 	PREFERRED_URL_SCHEME = 'https'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
