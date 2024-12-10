db9ec3ba7f76ce03dbac71e919bd0b12ca8a28fb
Va1a <n/a>
2024-12-09 20:02:39 -0800

    "

--- skedule/config.py
+++ skedule/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
 	SECRET_KEY = os.environ.get('SKEDULE_SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') if os.environ.get('DATABASE_URL') else 'none'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') if os.environ.get('DATABASE_URL') else 'sqlite:///site.db'
 	RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_SITE_KEY')
 	RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
 	RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
